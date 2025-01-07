8892e79895e1ec07be2bce4a0a1afa000a931161
cvemir369 <cvemir369@gmail.com>
2025-01-07 16:30:10 +0100

    btn test

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <h1 className="text-3xl">Hi Bro !!!</h1>
       <button className="btn">
         <svg
